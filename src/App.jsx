fa19b4c6e6c6132bb28a073a205e0b265acc3733
Gabriel Andrade <mendoncagabriel1997@gmail.com>
2023-09-16 20:40:07 -0300

    up

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
     <>
     <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/shopdosbaloes" element={<Home />} />
         </Routes>
       </BrowserRouter>
     </>
