09103119ead2ddf26194a2dd2f0f752e268ca9e6
Gabriel Andrade <138128244+MendoncaGabriel@users.noreply.github.com>
2023-09-16 12:12:20 -0400

    add

--- loja/src/App.jsx
+++ loja/src/App.jsx
@@ -6,7 +6,7 @@ function App() {
     <>
     <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/loja" element={<Home />} />
         </Routes>
       </BrowserRouter>
     </>
