bd081da6d69cc6a254cda152f71741e1187d5c9c
Gabriel Andrade <mendoncagabriel1997@gmail.com>
2023-09-16 20:25:40 -0300

    add

--- loja/src/App.jsx
+++ loja/src/App.jsx
@@ -6,7 +6,7 @@ function App() {
     <>
     <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="https://mendoncagabriel.github.io/shopdosbaloes/loja/" element={<Home />} />
         </Routes>
       </BrowserRouter>
     </>
