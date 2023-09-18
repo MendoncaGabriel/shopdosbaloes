16de5803c06bed65df13569ac17800d3cbb20be4
Gabriel Andrade <138128244+MendoncaGabriel@users.noreply.github.com>
2023-09-18 16:58:19 -0400

    add set de quantidade e cardItem

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,4 +1,6 @@
+import CartItem from "./CartItem"
 function Cart(){
+
     const cart = {
         width: '100%',
         height: '100%'
@@ -22,78 +24,10 @@ function Cart(){
                 {/* Itens do carrinho */}
                 <div className=" py-5 space-y-5">
 
-                    <div className=" grid grid-cols-4">
-                        <div className="">
-                            <img 
-                                src="https://camo.githubusercontent.com/b7b7dca15c743879821e7cfc14e8034ecee3588e221de0a6f436423e304d95f5/68747470733a2f2f7a7562652e696f2f66696c65732f706f722d756d612d626f612d63617573612f33363664616462316461323032353338616531333332396261333464393030362d696d6167652e706e67" 
-                                alt="imagem produto carrinho" 
-                                className="aspect-square rounded-lg w-20"
-                            />
-                        </div>
-
-
-                        <div className=" col-span-2 text-gray-600 pl-1">
-                            <h2>Nome do Produto</h2>
-                            <h2>Tamanho x</h2>
-                            <h2>R$9,99</h2>
-                        </div>
-
-                        <div className=" flex items-center justify-center">
-                            <button className=" border-2 w-4 h-7 flex items-center justify-center rounded-full">-</button>
-                            <input type="number" name="" id="" className=" w-9 h-5 p-1" />
-                            <button className=" border-black w-4 h-7 flex items-center justify-center rounded-full bg-black text-white pb-0">+</button>
-                        </div>
-
-
-                    </div>
-                    <div className=" grid grid-cols-4">
-                        <div className="">
-                            <img 
-                                src="https://camo.githubusercontent.com/b7b7dca15c743879821e7cfc14e8034ecee3588e221de0a6f436423e304d95f5/68747470733a2f2f7a7562652e696f2f66696c65732f706f722d756d612d626f612d63617573612f33363664616462316461323032353338616531333332396261333464393030362d696d6167652e706e67" 
-                                alt="imagem produto carrinho" 
-                                className="aspect-square rounded-lg w-20"
-                            />
-                        </div>
-
-
-                        <div className=" col-span-2 text-gray-600 pl-1">
-                            <h2>Nome do Produto</h2>
-                            <h2>Tamanho x</h2>
-                            <h2>R$9,99</h2>
-                        </div>
-
-                        <div className=" flex items-center justify-center">
-                            <button className=" border-2 w-4 h-7 flex items-center justify-center rounded-full">-</button>
-                            <input type="number" name="" id="" className=" w-9 h-5 p-1" />
-                            <button className=" border-black w-4 h-7 flex items-center justify-center rounded-full bg-black text-white pb-0">+</button>
-                        </div>
-
-
-                    </div>
-                    <div className=" grid grid-cols-4">
-                        <div className="">
-                            <img 
-                                src="https://camo.githubusercontent.com/b7b7dca15c743879821e7cfc14e8034ecee3588e221de0a6f436423e304d95f5/68747470733a2f2f7a7562652e696f2f66696c65732f706f722d756d612d626f612d63617573612f33363664616462316461323032353338616531333332396261333464393030362d696d6167652e706e67" 
-                                alt="imagem produto carrinho" 
-                                className="aspect-square rounded-lg w-20"
-                            />
-                        </div>
-
-
-                        <div className=" col-span-2 text-gray-600 pl-1">
-                            <h2>Nome do Produto</h2>
-                            <h2>Tamanho x</h2>
-                            <h2>R$9,99</h2>
-                        </div>
-
-                        <div className=" flex items-center justify-center">
-                            <button className=" border-2 w-4 h-7 flex items-center justify-center rounded-full">-</button>
-                            <input type="number" name="" id="" className=" w-9 h-5 p-1" />
-                            <button className=" border-black w-4 h-7 flex items-center justify-center rounded-full bg-black text-white pb-0">+</button>
-                        </div>
-
-
-                    </div>
+                    <CartItem />
+                    <CartItem />
+                    <CartItem />
+   
                 </div>
 
 
